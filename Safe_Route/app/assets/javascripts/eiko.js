419c848e20f15508d795b8de0438c0f4d8d385f6
Austin Bourdier <austinbourdier@gmail.com>
2014-07-25 00:33:02 -0700

    merging master

--- Safe_Route/app/assets/javascripts/eiko.js
+++ Safe_Route/app/assets/javascripts/eiko.js
@@ -2,22 +2,18 @@ $(document).ready(function() {
   var directionsDisplay;
   var directionsService = new google.maps.DirectionsService();
   var map;
-  initialize();
 
 function initialize() {
     directionsDisplay = new google.maps.DirectionsRenderer();
-    var SanFransisco = new google.maps.LatLng(37.7833, -122.4167);
+    var SanFransisco = new google.maps.LatLng(37.7583, -122.4367);
     var mapOptions = {
-      zoom:11,
+      zoom:13,
       center: SanFransisco
     }
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(document.getElementById("directionsPanel"));
   }
-
-
-
 
 
   function calcRoute(start, end) {
@@ -31,29 +27,29 @@ function initialize() {
     };
     directionsService.route(request, function(result, status) {
       if (status == google.maps.DirectionsStatus.OK) {
-
         directionsDisplay.setDirections(result)
       }
     });
   }
 
-
-  $('#locations').on('submit', function(event){
-
-    event.preventDefault();
-    $.ajax({
+  function crimeDataGrabber(){
+      $.ajax({
       url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2009-01-01&count=1000',
       type: 'GET',
-      dataType: 'json'
-    }).done(function(response){
-      console.log(response)
+      dataType: 'jsonp',
+      success:function(data){
+        console.log(data);
+      }
     })
+  }
+
+  $('#locations').on('submit', function(event){
+    event.preventDefault();
     var start = $(event.target).serializeArray()[0].value
     var end = $(event.target).serializeArray()[1].value
+    crimeDataGrabber()
     calcRoute(start, end);
-    
   })
   initialize();
-
 })
 
