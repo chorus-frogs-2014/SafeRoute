1b50afe3f1592eda934bfa4e6d735497092e5357
Scott Jason <scottleejason@gmail.com>
2014-07-25 16:40:03 -0700

    completed refactor v1 for mvc maps, added widget for crimes

--- Safe_Route/app/assets/javascripts/MapsWidget/maps-controller.js
+++ Safe_Route/app/assets/javascripts/MapsWidget/maps-controller.js
@@ -3,11 +3,17 @@ var SafeRoute = SafeRoute || {}
 SafeRoute.MapsController = {
     initialize: function(model, view, directionsService, directionsDisplay){
     var sanFranGoogleObj = new google.maps.LatLng(37.7583, -122.4367);
-    this.model = model
+    this.model = model;
     this.view = view;
-    this.view.render(directionsDisplay, sanFranGoogleObj);
-  }
+    this.view.renderMap(directionsDisplay, sanFranGoogleObj);
+    this.view.bindListeners(this, directionsDisplay, directionsService);
+  },
+    fetchCoords: function(start, end, directionsDisplay, directionsService){
+      this.model.request(this, start, end, directionsDisplay, directionsService)
+  },
+    collectCoords: function(result, directionsDisplay){
+      this.view.renderRoutes(result, directionsDisplay);
+    }
 }
-
 
 
--- Safe_Route/app/assets/javascripts/init.js
+++ Safe_Route/app/assets/javascripts/init.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var directionsService = new google.maps.DirectionsService();
   var directionsDisplay = new google.maps.DirectionsRenderer();
-
   SafeRoute.MapsController.initialize(SafeRoute.MapsModel, SafeRoute.MapsView, directionsService, directionsDisplay);
 });
+
 
