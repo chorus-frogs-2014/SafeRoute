6d9f1eb7cdd5cc7b5c62c7556711fcc6599557b0
Scott Jason <scottleejason@gmail.com>
2014-07-26 04:04:03 -0700

    this branch can be deleted

--- Safe_Route/app/assets/javascripts/CrimesWidget/crimes-model.js
+++ Safe_Route/app/assets/javascripts/CrimesWidget/crimes-model.js
@@ -1,38 +1,22 @@
 SafeRoute.CrimesModel = {
 	crimeDataGrabber: function(crimeDataResult){
 			var self = this
-		 $.ajax({
+		 $.when( $.ajax({
 			url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2009-01-01&count=3500',
 			type: 'GET',
-			dataType: 'jsonp',
-			success:function(data){	
-				self.crimeDataResult(data)
+			dataType: 'jsonp'
 			}
 		})
-	},
+	).done
+},
 	crimeDataResult: function(data){
 			var data = data
 			// console.log(data)
-			SafeRoute.RoutesController.analyzeCrimeAndRouteData(data, result, start, end)
 			return data
 	}
 }
 
-
-// function testAjax(handleData) {
-//   $.ajax({
-//     url:"getvalue.php",  
-//     success:function(data) {
-//       handleData(data); 
-//     }
-//   });
-// }
-
-
-
-
-// Call it like this:
-
-// testAjax(function(output){
-//   // here you use the output
-// });
+$.when( doAjax(), doMoreAjax() )
+  .done(function() {
+    console.log( 'I fire once BOTH ajax requests have completed!' );
+  })
