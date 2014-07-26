c6cc2f1a6a5687bf74a9cb89f58ff99a03baa269
Scott Jason <scottleejason@gmail.com>
2014-07-26 04:31:17 -0700

    refactored naming conventions in crimes widget

--- Safe_Route/app/assets/javascripts/MapsWidget/maps-controller.js
+++ Safe_Route/app/assets/javascripts/MapsWidget/maps-controller.js
@@ -12,7 +12,7 @@ SafeRoute.MapsController = {
       this.model.requestRoutes(this, start, end)
   },
     collect: function(result, start, end){
-      SafeRoute.CrimesController.crimeApiCall(result, start, end)
+      SafeRoute.CrimesController.request(result, start, end)
   }
 }
 
