80739d6c670115b07b0bee903c775346285cb61f
Austin Bourdier <austinbourdier@gmail.com>
2014-07-26 03:53:51 -0700

    ajax return data working

--- Safe_Route/app/assets/javascripts/CrimesWidget/crimes-controller.js
+++ Safe_Route/app/assets/javascripts/CrimesWidget/crimes-controller.js
@@ -4,10 +4,11 @@ SafeRoute.CrimesController = {
 		this.model = model;
 	},
 	crimeApiCall: function(result, start, end){
-		this.sendToRoutes(result, start, end, this.model.crimeDataGrabber())
+		this.model.crimeDataGrabber(this, result,start, end)
+
 	},
 	sendToRoutes: function(result, start, end, data){
-		console.log(data);
-		// SafeRoute.RoutesController.analyzeCrimeAndRouteData(data, result, start, end)
+		console.log(data)
+		// SafeRoute.RoutesController.analyzeCrimeAndRouteData(data, result, start, end);
 	}
 }
--- Safe_Route/app/assets/javascripts/CrimesWidget/crimes-model.js
+++ Safe_Route/app/assets/javascripts/CrimesWidget/crimes-model.js
@@ -1,38 +1,12 @@
 SafeRoute.CrimesModel = {
-	crimeDataGrabber: function(crimeDataResult){
-			var self = this
+	crimeDataGrabber: function(controller, result,start, end){
 		 $.ajax({
 			url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2009-01-01&count=3500',
 			type: 'GET',
-			dataType: 'jsonp',
-			success:function(data){	
-				self.crimeDataResult(data)
-			}
+			dataType: 'jsonp'
+		}).done(function(data){
+			controller.sendToRoutes(result,start, end, data)
 		})
-	},
-	crimeDataResult: function(data){
-			var data = data
-			// console.log(data)
-			SafeRoute.RoutesController.analyzeCrimeAndRouteData(data, result, start, end)
-			return data
 	}
 }
 
-
-// function testAjax(handleData) {
-//   $.ajax({
-//     url:"getvalue.php",  
-//     success:function(data) {
-//       handleData(data); 
-//     }
-//   });
-// }
-
-
-
-
-// Call it like this:
-
-// testAjax(function(output){
-//   // here you use the output
-// });
