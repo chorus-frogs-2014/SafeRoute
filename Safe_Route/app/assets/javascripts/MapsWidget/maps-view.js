273e46da069dfd7930c21dc64c0a3681c0d623dd
Austin Bourdier <austinbourdier@gmail.com>
2014-07-26 22:51:28 -0700

    enabled default google maps ui stuff

--- Safe_Route/app/assets/javascripts/MapsWidget/maps-view.js
+++ Safe_Route/app/assets/javascripts/MapsWidget/maps-view.js
@@ -1,7 +1,6 @@
 SafeRoute.MapsView = {
   renderMap: function(controller, directionsDisplay, sanFranGoogleObj){
     directionsDisplay.setMap(new google.maps.Map(document.getElementById("map-canvas"), {
-      disableDefaultUI: true,
       zoom:13,
       center: sanFranGoogleObj
     }));
