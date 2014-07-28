235128f3717ca98d1c4f07adec316f32f4ca915f
Scott Jason <scottleejason@gmail.com>
2014-07-27 20:53:52 -0700

    master controller displaying source and target

--- Safe_Route/app/assets/javascripts/init.js
+++ Safe_Route/app/assets/javascripts/init.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
   var directionsDisplay = new google.maps.DirectionsRenderer();
 
   SafeRoute.MapsController.initialize(SafeRoute.MapsModel, SafeRoute.MapsView);
-  SafeRoute.MapsModel.initialize(directionsService, SafeRoute.RoutesController);
+  SafeRoute.MapsModel.initialize(directionsService, SafeRoute.MasterController);
 
 
   SafeRoute.CrimesController.initialize(SafeRoute.CrimesModel);
 
-  SafeRoute.RoutesController.initialize(SafeRoute.RoutesModel, SafeRoute.RoutesView, SafeRoute.MapsController, SafeRoute.MapsView, SafeRoute.CrimesController, directionsDisplay);
-  SafeRoute.RoutesModel.initialize(directionsService, directionsDisplay);
+  SafeRoute.MasterController.initialize(SafeRoute.RoutesModel, SafeRoute.RoutesView, SafeRoute.MapsController, SafeRoute.MapsView, SafeRoute.CrimesController, directionsDisplay);
+  // SafeRoute.RoutesModel.initialize(directionsService, directionsDisplay);
 });
