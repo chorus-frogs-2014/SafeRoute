762406bcfeadcaf3ac8c01df8fb53f40d1092c2c
Scott Jason <scottleejason@gmail.com>
2014-07-29 11:09:11 -0700

    only the master controller is initialized on document ready

--- Safe_Route/javascripts/init.js
+++ Safe_Route/javascripts/init.js
@@ -1,36 +1,3 @@
-var getPositonSuccess = function(position){
-    var currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'latLng': currentLatLng}, geocoderCB)
-  }
-
-  var geocoderCB = function(results, status){
-    if (status == google.maps.GeocoderStatus.OK){
-      showAddress(results);
-    } else {
-      showError();
-    }
-  }
-
-  var showAddress = function(results){
-    $('input').eq(0).val(results[0].formatted_address)
-  }
-
-  var showError = function(){
-    alert("Sorry! Couldn't get your current location." )
-  }
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPositonSuccess, showError);
-  } else {
-    showError();
-  }
-
 $(document).ready(function(){
-  var directionsService = new google.maps.DirectionsService();
-  var directionsDisplay = new google.maps.DirectionsRenderer();
-  SafeRoute.MasterController.initialize(SafeRoute.MapsController, SafeRoute.MapsModel, SafeRoute.MapsView, SafeRoute.CrimesController, SafeRoute.RoutesController, SafeRoute.RoutesView, directionsDisplay);
-  SafeRoute.MapsModel.initialize(directionsService);
-  SafeRoute.CrimesController.initialize(SafeRoute.CrimesModel);
-  SafeRoute.RoutesController.initialize(SafeRoute.RoutesModel, SafeRoute.RoutesView);
-  SafeRoute.RoutesModel.initialize(directionsService, directionsDisplay);
+  SafeRoute.MasterController.initialize(SafeRoute.MapsController, SafeRoute.MapsModel, SafeRoute.MapsView, SafeRoute.CrimesController, SafeRoute.CrimesModel, SafeRoute.RoutesController, SafeRoute.RoutesModel, SafeRoute.RoutesView);
 });
--- Safe_Route/javascripts/master-controller.js
+++ Safe_Route/javascripts/master-controller.js
@@ -1,15 +1,22 @@
 var SafeRoute = SafeRoute || {}
 SafeRoute.MasterController = {
-    initialize: function(MapsController, MapsModel, MapsView, CrimesController, RoutesController, RoutesView, directionsDisplay) {
+    initialize: function(MapsController, MapsModel, MapsView, CrimesController, CrimesModel, RoutesController, RoutesModel, RoutesView) {
         this.sanFranGoogleObj = new google.maps.LatLng(37.7583, -122.4367);
+        this.directionsDisplay = new google.maps.DirectionsRenderer();
+        this.directionsService = new google.maps.DirectionsService();
         this.MapsController = MapsController;
         this.MapsModel = MapsModel;
         this.MapsView = MapsView;
         this.CrimesController = CrimesController;
+        this.CrimesModel = CrimesModel;
         this.RoutesController = RoutesController;
+        this.RoutesModel = RoutesModel;
         this.RoutesView = RoutesView;
-        this.directionsDisplay = directionsDisplay;
         this.MapsController.initialize(this.MapsModel, this.MapsView);
+        this.MapsModel.initialize(this.directionsService);
+        this.CrimesController.initialize(this.CrimesModel);
+        this.RoutesController.initialize(this.RoutesModel, this.RoutesView);
+        this.RoutesModel.initialize(this.directionsService, this.directionsDisplay);
         this.RoutesView.initialize(this.sanFranGoogleObj);
         this.run();
     },
