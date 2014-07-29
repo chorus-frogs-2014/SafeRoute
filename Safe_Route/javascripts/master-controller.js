9d14a225267dfdee69c759b5dd2aa0b8185c9a92
Scott Jason <scottleejason@gmail.com>
2014-07-28 20:58:47 -0700

    resized map

--- Safe_Route/javascripts/master-controller.js
+++ Safe_Route/javascripts/master-controller.js
@@ -1,28 +1,28 @@
 var SafeRoute = SafeRoute || {}
 SafeRoute.MasterController = {
-  initialize: function(MapsController, MapsView, CrimesController, RoutesController, directionsDisplay){
-    this.sanFranGoogleObj = new google.maps.LatLng(37.7583, -122.4367);
-    this.MapsController = MapsController;
-    this.MapsView = MapsView;
-    this.CrimesController = CrimesController;
-    this.RoutesController = RoutesController;
-    this.directionsDisplay = directionsDisplay;
-    this.run();
-  },
-  run: function(){
-      this.bindListeners();
-      alert("I work!")
-  this.MapsView.render(this.directionsDisplay, this.sanFranGoogleObj);
+    initialize: function(MapsController, MapsView, CrimesController, RoutesController, directionsDisplay) {
+        this.sanFranGoogleObj = new google.maps.LatLng(37.7583, -122.4367);
+        this.MapsController = MapsController;
+        this.MapsView = MapsView;
+        this.CrimesController = CrimesController;
+        this.RoutesController = RoutesController;
+        this.directionsDisplay = directionsDisplay;
+        this.run();
     },
-  bindListeners: function(){
-   $('#locations').on('submit', this.MapsController.collectCoords);
-  },
-  collectMapData: function(mapsData){
-    this.mapsData = mapsData
-    this.CrimesController.request();
-  },
-  collectCrimeData: function(crimesData){
-    this.crimesData = crimesData
-    this.RoutesController.collectMapAndCrimeData(this.mapsData, this.crimesData)
-  }
+    run: function() {
+        this.bindListeners();
+        alert("I work!")
+        this.MapsView.render(this.directionsDisplay, this.sanFranGoogleObj);
+    },
+    bindListeners: function() {
+        $('#locations').on('submit', this.MapsController.collectCoords);
+    },
+    collectMapData: function(mapsData) {
+        this.mapsData = mapsData
+        this.CrimesController.request();
+    },
+    collectCrimeData: function(crimesData) {
+        this.crimesData = crimesData
+        this.RoutesController.collectMapAndCrimeData(this.mapsData, this.crimesData)
+    }
 }
