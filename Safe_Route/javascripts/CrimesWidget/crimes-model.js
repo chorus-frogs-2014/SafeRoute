654dc6e6e95ec0016f6dfb0735bac4650efeb7ec
Austin Bourdier <austinbourdier@gmail.com>
2014-07-30 00:50:36 -0700

    implemented heat map a+ extra styling

--- Safe_Route/javascripts/CrimesWidget/crimes-model.js
+++ Safe_Route/javascripts/CrimesWidget/crimes-model.js
@@ -2,7 +2,7 @@ SafeRoute.CrimesModel = {
   requestCrimes: function(controller){
     $('#fountainG').show();
      $.ajax({
-      url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2009-01-01&count=3500',
+      url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2009-01-01&count=10000',
       type: 'GET',
       dataType: 'jsonp'
     }).done(function(data){
