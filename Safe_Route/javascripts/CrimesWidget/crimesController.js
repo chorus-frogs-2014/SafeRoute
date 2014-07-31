17a8109efd94e8857c1ae9f76564169da826eba6
Scott Jason <scottleejason@gmail.com>
2014-07-31 14:17:22 -0700

    application prepped and ready for thurs presentation practice

--- Safe_Route/javascripts/CrimesWidget/crimesController.js
+++ Safe_Route/javascripts/CrimesWidget/crimesController.js
@@ -5,9 +5,4 @@ SafeRoute.CrimesController = {
   request: function(){
     this.model.requestCrimes(this)
   }
-  // collect: function(data){
-  //   crimesData = [];
-  //   crimesData.push(data);
-  // $(this).trigger('collectCrimes', [crimesData]).bind(this);
-  // }
 }
