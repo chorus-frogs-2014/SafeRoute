37dc025da118ef6e66571c9f68aa3eb10007b7d1
Scott Jason <scottleejason@gmail.com>
2014-07-31 12:59:57 -0700

    completed loading animations

--- Safe_Route/javascripts/CrimesWidget/crimesModel.js
+++ Safe_Route/javascripts/CrimesWidget/crimesModel.js
@@ -1,6 +1,7 @@
 SafeRoute.CrimesModel = {
   requestCrimes: function(controller){
     $('#fountainG').show();
+    $('#loading').addClass('animated zoomIn');
     $.ajax({
       url: 'http://sanfrancisco.crimespotting.org/crime-data?format=json&dstart=2006-01-01&count=3500&type=AA,Mu,Ro,SA,Al,Th',
       type: 'GET',
@@ -8,6 +9,7 @@ SafeRoute.CrimesModel = {
     }).done(function(data){
       $('#locations').addClass('flex')
       $('#loading').hide();
+      $('#loadingImage').hide();
       $(this).trigger('collectCrimes', [data])
     }.bind(this))
   }
